9e49dd7ce9f4fe92d683525a672ee5e5e3b63101
mightymancalvin <mightymanmensah@gmail.com>
2023-01-20 16:27:41 +0000

    modified views and urls patterns in demo Projects

--- demo-project/demo_project_webapp/demo_app/views.py
+++ demo-project/demo_project_webapp/demo_app/views.py
@@ -3,4 +3,20 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
+    print(request.method)
+    for item in request.headers:
+        print(item)
     return HttpResponse("Hello!, This is the Welcome Home for DemoApp")
+
+
+def drinks(request, drink_name):
+
+    drink = {
+        'mocha': 'type of coffee',
+        'tea' : 'type of beverage',
+        'lemonade' : 'type of refreshment',
+    }
+    choice_of_drink = drink[drink_name]
+
+    return HttpResponse(f"<h2> {drink_name} </h2>" + choice_of_drink)
+
